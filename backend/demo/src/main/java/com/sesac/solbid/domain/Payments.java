e39fb33097841c705826d05def381e6518f64d43
porcupine7021 <lovewl1021@naver.com>
2025-08-01 11:54:28 +0900

    payment fix

--- backend/demo/src/main/java/com/sesac/solbid/domain/Payments.java
+++ backend/demo/src/main/java/com/sesac/solbid/domain/Payments.java
@@ -1,4 +1,6 @@
-`package com.sesac.solbid.domain;import com.sesac.solbid.domain.baseentity.BaseEntity;
+package com.sesac.solbid.domain;
+import com.sesac.solbid.domain.User;
+import com.sesac.solbid.domain.baseentity.BaseEntity;
 import com.sesac.solbid.domain.enums.PaymentStatus;
 import jakarta.persistence.*;
 import org.hibernate.annotations.ColumnDefault;
@@ -12,23 +14,32 @@ public class Payments extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "payment_id", length = 50, nullable = false)
     private Long paymentId;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
+
     @Column(name = "amount", nullable = false)
     private int amount;
+
     @Column(name = "payment_method", length = 20, nullable = false)
     private String paymentMethod;
+
     @Column(name = "transaction_id", length = 100)
     private String transactionId;
+
     @Enumerated(EnumType.STRING)
     private PaymentStatus paymentStatus;
+
     @ColumnDefault("0")
     @Column(name = "converted_point", nullable = false)
     private int convertedPoint;
+
     @Column(name = "is_charged", nullable = false)
     @ColumnDefault("false")
     private Boolean isCharged;
+
     private LocalDateTime requestedAt;
+
     private LocalDateTime confirmedAt;
-}`
+}
--- backend/demo/src/main/java/com/sesac/solbid/domain/PointTransaction.java
+++ backend/demo/src/main/java/com/sesac/solbid/domain/PointTransaction.java
@@ -1,6 +1,6 @@
-`package com.sesac.solbid.domain;import com.sesac.solbid.domain.enums.TransEnum;
+package com.sesac.solbid.domain;
+import com.sesac.solbid.domain.enums.TransEnum;
 import jakarta.persistence.*;
-
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
@@ -11,20 +11,27 @@ public class PointTransaction {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "transaction_id")
     private Long transactionId;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
+
     @Enumerated(EnumType.STRING)
     private TransEnum transEnum;
+
     @Column(unique = true, nullable = false)
     private BigDecimal balanceAfter;
+
     @Column(length = 225)
     private String description;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "payment_id")
     private Payments payment_id;
+
     @Column(unique = true, nullable = false)
     private LocalDateTime createdAt;
+
     @Column(name = "point", nullable = false)
     private int point;
 }`
